b76c4045bcdb4f76d6a61cbc03a117848dd8140b
David <josedago1163@gmail.com>
2023-03-27 20:25:41 -0500

    a

--- spotify.py
+++ spotify.py
@@ -1,22 +1,35 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
-from flask import Flask, render_template, request
+from flask import request
 import openai
-
-app = Flask(__name__, template_folder="templates")
+import json
+import requests
+import base64
+import urllib.parse
+from flask import request
 
 #authentication stuff
 openai.api_key = "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"
 
-spotify_client_id = "e68285acd05e49bb9134e5bcc2622778"
-spotify_client_secret = "9c30bbe0f3ed49ae83dab84643141535"
+# Client Keys
+CLIENT_ID = "e68285acd05e49bb9134e5bcc2622778"
+CLIENT_SECRET = "9c30bbe0f3ed49ae83dab84643141535"
 
-scope = "user-library-read"
+#Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = f'{SPOTIFY_API_BASE_URL}/{API_VERSION}'
 
-spotify_client_credentials_manager = SpotifyClientCredentials(
-    client_id=spotify_client_id,
-    client_secret=spotify_client_secret
-)
+SCOPE = "user-library-read"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+#Server-side Parameters
+def get_redirect_uri():
+    URL_URI = request.url_root
+    REDIRECT = f"{URL_URI}callback"
+    return REDIRECT
 
 def app_Authorization():
     REDIRECT_URI = get_redirect_uri()
@@ -32,19 +45,38 @@ def app_Authorization():
     auth_url = f"{SPOTIFY_AUTH_URL}/?{url_args}"
     return auth_url
 
-@app.route("/")
-def home():
-    
-    return render_template("index.html")
+def user_Authorization():
+    REDIRECT_URI = get_redirect_uri()
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+    client_str = f'{CLIENT_ID}:{CLIENT_SECRET}'
+    client_encode = base64.b64encode(client_str.encode("utf-8"))
+    client_encode = str(client_encode, "utf-8")
+    headers = {"Authorization": f"Basic {client_encode}"}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
 
+    # Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
 
+    # Use the access token to access Spotify API
+    authorization_header = {"Authorization":f"Bearer {access_token}"}
+    return authorization_header
 
-@app.route("/callback")
-def callback():
-    spotify = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(scope=scope, client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri="http://localhost:8888/callback")
-    )
-    result = spotify.current_user_saved_tracks(scope=scope)
-    return result
+# Get user albums data
+def get_user_liked_songs():
+    authorization_header = user_Authorization()
+    # Get profile data
+    user_profile_api_endpoint = f"{SPOTIFY_API_URL}/me/tracks"
+    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    profile_data = json.loads(profile_response.text)
+    return profile_data
 
 
